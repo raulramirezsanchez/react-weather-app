6f3bf842db70cc051ce62f3e692e0df158c6df57
Raul Ramirez <Raul.Ramirez@kantar.com>
2022-10-27 17:29:25 +0200

    first version

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0643fee50dmsh609ac865e3ecc84p1104eajsn47e2740e4156',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3626d54606bfe92afafbcbf0e919d07b";
